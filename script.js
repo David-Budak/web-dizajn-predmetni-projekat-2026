626da44f710ad05d5a51dbb12134f85752f830b4
alennez <backupgavrilovic2111@gmail.com>
2026-02-03 19:35:58 +0100

    SKRIPTA STA NIOJE U REDU S NJOM??????

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // David Budak, AI 7/2022
 // Alen Gavrilović AI 13/2022
 
+function sendMail(){
+
+
+            let parms = {
+                name : document.getElementById("name").value,
+                email : document.getElementById("email").value,
+                subject : document.getElementById("subject").value,
+                message : document.getElementById("message").value,
+
+}
+
+emailjs.send("service_z2l128i","template_htstmys", parms).then(alert("Email Sent!!"))
+
+}
